9a03c5e10b7c6b00734db367c6d3d5c12b090e5f
Donato Robledo <129457178+MiniDonny@users.noreply.github.com>
2023-07-14 22:05:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const header = () => document.querySelector("header").innerHTML += `
+<div class="container_header">
+                <div class="menu">
+                    <input type="checkbox" id="check_menu">
+                    <label id="label_check" for="check_menu">
+                        <i class="fa-solid fa-bars icon_burger"></i>
+                    </label>
+                    <nav>
+                        <ul>
+                            <li><a href="#">HOME</a></li>
+                            <li><a href="#">ABOUT ME</a></li>
+                            <li><a href="#">KNOWLEDGE</a></li>
+                            <li><a href="#">PROJECTS</a>
+                                <ul class="submenu">
+                                    <li><a href="#">Mini Donny</a></li>
+                                    <li><a href="#">Data Base</a></li>
+                                    <li><a href="#">Little Button</a></li>
+                                    <li><a href="#">My Portfolio</a></li>
+                                    <li><a href="#">Shop Skateboards</a></li>
+                                    <li><a href="#">Animations</a></li>
+                                    <li><a href="#">Header and Footer</a></li>
+                                    <li><a href="#">Cirriculum Vitae Venom</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    <nav>
+                </div>
+            </div>
+`
+header()
