20e06b31669ae2b29cb05d399b6b720cf9f27b86
Donato Robledo <129457178+MiniDonny@users.noreply.github.com>
2023-07-27 21:52:34 -0300

    Progress of the main

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 /*const header = () => document.querySelector("header").innerHTML += `
-<div class="container_header_up">
-    <h1>Donato Robledo</h1>
-    <img class="logo" src="./imagenes/Logo.jpg" alt="">
-    </div>
-    <div class="container_header">
-    <div class="menu">
-        <input type="checkbox" id="check_menu">
-        <label id="label_check" for="check_menu">
-            <i class="fa-solid fa-bars icon_burger"></i>
-        </label>
-        <nav>
-            <ul>
-                <li><a href="home">HOME</a></li>
-                <li><a href="#">KNOWLEDGE</a></li>
-                <li><a href="#">PROJECTS</a>
-                    <ul class="submenu">
-                        <li><a href="#">Mini Donny</a></li>
-                        <li><a href="#">Data Base</a></li>
-                        <li><a href="#">Little Button</a></li>
-                        <li><a href="#">My Portfolio</a></li>
-                        <li><a href="#">Shop Skateboards</a></li>
-                        <li><a href="#">Animations</a></li>
-                        <li><a href="#">Header and Footer</a></li>
-                        <li><a href="#">Cirriculum Vitae Venom</a></li>
-                    </ul>
-                </li>
-            </ul>
-        <nav>
-    </div>
-    </div>
+ <div class="container_header_up">
+                <h1>Donato Robledo</h1>
+                <img class="logo" src="./imagenes/logo.png" alt="">
+                </div>
+                <div class="container_header">
+                <div class="menu">
+                    <input type="checkbox" id="check_menu">
+                    <label id="label_check" for="check_menu">
+                        <i class="fa-solid fa-bars icon_burger"></i>
+                    </label>
+                    <nav>
+                        <ul>
+                            <li><a href="#home">HOME</a></li>
+                            <li><a href="#knowledge">KNOWLEDGE</a></li>
+                            <li><a href="#">PROJECTS</a>
+                                <ul class="submenu">
+                                    <li><a href="#">MiniDonny</a></li>
+                                    <li><a href="#">My-Portfolio</a></li>
+                                    <li><a href="#">Little-Button</a></li>
+                                    <li><a href="#">Data-Base</a></li>
+                                    <li><a href="#">Input-JS</a></li>
+                                    <li><a href="#">Animations</a></li>
+                                    <li><a href="#">Header-And-Footer</a></li>
+                                    <li><a href="#">Git-With-Github</a></li>
+                                    <li><a href="#">Cirriculum-Vitae-Venom</a></li>
+                                    <li><a href="#">Shop-Skateboards</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    <nav>
+                </div>
+                </div>
 `
 header()
-
 window.addEventListener("scroll", function(){
     var header = this.document.querySelector("header");
     header.classList.toggle("abajo",window.scrollY>0); 
