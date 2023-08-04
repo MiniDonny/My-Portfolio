87cd43b7fdf9536d460b6259cd3e82a8086b4440
Donato Robledo <129457178+MiniDonny@users.noreply.github.com>
2023-08-04 13:43:45 -0300

    Bug fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*const header = () => document.querySelector("header").innerHTML += `
+const header = () => document.querySelector("header").innerHTML += `
  <div class="container_header_up">
                 <h1>Donato Robledo</h1>
                 <img class="logo" src="./imagenes/logo.jpg" alt="logo">
@@ -28,75 +28,8 @@
                 </div>
                 </div>
 `
-
-const main = () => document.querySelector("main").innerHTML +=`
-<section class="home" id="home">
-                <article class="myselfpresentation">
-                    <h2>HELLOW! I'M <div class="myname">DONATO ROBLEDO</div></h2>
-                    <h4>a young programmer who inspires to be Frontend and Full Stack</h4>
-                </article>
-                <article class="myserlfimg">
-                    <img src="./imagenes/myself.jpg" alt="">
-                </article>
-           </section>
-           <section class="knowledge" id="knowledge"> 
-                <h2>My Knowledges</h2>
-                <h3>These are the languages and technologies that I use so far. Although, I am always learning new things.</h3>
-                <div class="containerArticles">
-                    <article>
-                        <img src="./imagenes/html-icon.png" alt="html-icon">
-                        <p>HTML</p>
-                    </article>
-                    <article>
-                        <img src="./imagenes/css-icon.png" alt="css-icon">
-                        <p>CSS</p>
-                    </article>
-                    <article>
-                        <img src="./imagenes/java-script-icon.png" alt="java-script-icon">
-                        <p>JAVA SCRIPT</p>
-                    </article>
-                    <article>
-                        <img src="./imagenes/java-icon.png" alt="java-icon">
-                        <P>JAVA</P>
-                    </article>
-                    <article>
-                        <img src="./imagenes/mysql-icon.png" alt="mysql-icon">
-                        <P>MYSQL</P>
-                    </article>
-                    <article>
-                        <img src="./imagenes/github-icon.png" alt="github-icon">
-                        <p>GITHUB</p>
-                    </article>
-                </div>
-           </section>
-           <section class="projects" id="projects">
-                <h2>My Projects</h2>
-                <h3>Here you can find my finished projects that I developed during my training</h3>
-                <div class="container_articles">   
-                    <article class="minidonny" id="minidonny">
-                        <img src="./imagenes/readme.png" alt="img-readme">
-                        <p> <a href="https://github.com/MiniDonny/MiniDonny">MiniDonny</a></p>
-                    </article>
-                    <article class="my-portfolio" id="my-portfolio">
-                        <img src="./imagenes/my-portfolio.png" alt="img-my-portfolio">
-                        <p><a href="https://minidonny.github.io/My-Portfolio/">My-Portfolio</a></p>
-                    </article>
-                    <article class="data-base" id="data-base">
-                        <img src="./imagenes/data-base.png" alt="img-data-base">
-                        <p><a href="https://github.com/MiniDonny/Data-Base">Data-Base</a></p>
-                    </article>
-                    <article class="git-with-github" id="git-with-github">
-                        <img src="./imagenes/git-with-github.png" alt="img-git-with-github">
-                        <p><a href="https://github.com/MiniDonny/Git-With-Github">Git-With-Github</a></p>
-                    </article>
-                    <article class="curriculum-vitae-venom" id="curriculum-vitae-venom">
-                        <img src="./imagenes/curriculum-vitae-venom.png" alt="img-curriculum-vitae-venom">
-                        <p><a href="https://minidonny.github.io/Curriculum-Vitae-Venom/">Curriculum-Vitae-Venom</a></p>
-                    </article>
-                    <article class="shop-skateboards" id="shop-skateboards">
-                        <img src="./imagenes/shop-skateboards.png" alt="img-shop-skateboards">
-                        <p><a href="https://minidonny.github.io/Shop-Skateboards/">Shop-Skateboards</a></p>
-                    </article>
-                </div>     
-           </section>
-`*/
+header()
+window.addEventListener("scroll", function(){
+    var header = this.document.querySelector("header");
+    header.classList.toggle("abajo",window.scrollY>0); 
+})
